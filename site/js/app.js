87ed7bebb6f2eeae63377eec0f8303f694f8067c
Chris Hamm <chris.m.hamm@gmail.com>
2015-07-21 14:24:46 -0400

    velocity animation in progress

---
+++ site/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    
+
+$("button.nav-button home").velocity("transition.bounceDownIn", { duration: 800, delay: 125 });
+$("button.nav-button about").velocity({translateY: [0, -1000] }, { duration: 800, delay: 250, easing: [300,20] });
+$("button.nav-button contact").velocity({translateY: [0, -1000] }, { duration: 800, delay: 375, easing: [300,20] });
+
+    
+$(".button").mouseenter(function(){
+	$(this)
+		.velocity( { translateY: "-10px" }, 400 );
+});
+
+$(".button").mouseleave(function(){
+	$(this)
+		.velocity( { translateY: "0" }, 200 );
+});
+    
+    
+    
+    
+});
